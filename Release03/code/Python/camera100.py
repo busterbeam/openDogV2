0a77486983ea319a728b1b2da72760aa6ab16c42
Nath <nathanlloyd@protonmail.com>
2021-07-09 16:48:22 +0100

    Update camera100.py
    not brilliant, but I can see your coming from an Arduino environment so you script quite different. Your setup didn't require outputting if the initial key word is set.  Do try to use keywords.  `global` keyword is used inside functions when you want to use the variable defined in the global scope.  Everything in the global scope is global you don't have to define it as global.  `from * import *` is cleaner and shorter in larger scripts.  I imagine you will utilize all the detections in each "frame", but for now you seem to be only looking for one detection in each frame.  Argurment Parser is OK, but if this is running on a device from boot, you would be better off with making a configuration file and reading from it at initialization, look into `.ini` files and `configparser`.  `sys.exit()` is pretty much the same as `exit()`.  Your `try/except` statement should really catch a specific `Exception`

--- Release03/code/Python/camera100.py
+++ Release03/code/Python/camera100.py
@@ -1,176 +1,128 @@
-import RPi.GPIO as GPIO
-import jetson.inference
-import jetson.utils
+from RPi.GPIO import HIGH, LOW, BCM, output, setup
+from jetson.inference import detectNet
+from jetson.utils import videoSource, videoOutput, logUsage
 
-import time
+from argparse import ArgumentParser, RawTextHelpFormatter
+from sys import argv, exit
 
-import argparse
-import sys
+MOVE_BACK = 18
+MOVE_FORWARD = 17
+MOVE_LEFT = 16
+MOVE_RIGHT = 20
+MOVE_UP = 21
+MOVEMENTS = [MOVE_BACK, MOVE_FORWARD, MOVE_LEFT, MOVE_RIGHT, MOVE_UP]
 
-# parse the command line
-parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.", 
-                                 formatter_class=argparse.RawTextHelpFormatter, epilog=jetson.inference.detectNet.Usage() +
-                                 jetson.utils.videoSource.Usage() + jetson.utils.videoOutput.Usage() + jetson.utils.logUsage())
-
-parser.add_argument("input_URI", type=str, default="", nargs='?', help="URI of the input stream")
-parser.add_argument("output_URI", type=str, default="", nargs='?', help="URI of the output stream")
-parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre-trained model to load (see below for options)")
-parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
-parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use") 
-
-is_headless = ["--headless"] if sys.argv[0].find('console.py') != -1 else [""]
-
-try:
-	opt = parser.parse_known_args()[0]
-except:
-	print("")
-	parser.print_help()
-	sys.exit(0)
-
-# load the object detection network
-net = jetson.inference.detectNet(opt.network, sys.argv, opt.threshold)
-
-# create video sources & outputs
-input = jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
-output = jetson.utils.videoOutput(opt.output_URI, argv=sys.argv+is_headless)
-
-#setup GPIO pins
-
-GPIO.setmode(GPIO.BCM) #RaspPi pin numbering
-
-GPIO.setup(18, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.output(18, GPIO.HIGH)
-GPIO.setup(17, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.output(17, GPIO.HIGH)
-GPIO.setup(16, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.output(16, GPIO.HIGH)
-GPIO.setup(20, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.output(20, GPIO.HIGH)
-GPIO.setup(21, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.output(21, GPIO.HIGH)
-
-def back():
-	GPIO.output(18, GPIO.LOW)
-	GPIO.output(17, GPIO.HIGH)
-	GPIO.output(16, GPIO.HIGH)
-	GPIO.output(20, GPIO.HIGH)
-	GPIO.output(21, GPIO.HIGH)
+def back:
+	output(MOVE_BACK, LOW)
 	print("back")
 
-def forward():
-	GPIO.output(18, GPIO.HIGH)
-	GPIO.output(17, GPIO.LOW)
-	GPIO.output(16, GPIO.HIGH)
-	GPIO.output(20, GPIO.HIGH)
-	GPIO.output(21, GPIO.HIGH)
+def forward:
+	output(MOVE_FORWARD, LOW)
 	print("forward")
 
-def left():
-	GPIO.output(18, GPIO.HIGH)
-	GPIO.output(17, GPIO.HIGH)
-	GPIO.output(16, GPIO.LOW)
-	GPIO.output(20, GPIO.HIGH)
-	GPIO.output(21, GPIO.HIGH)
+def left:
+	output(MOVE_LEFT, LOW)
 	print("left")
 
-def right():
-	GPIO.output(18, GPIO.HIGH)
-	GPIO.output(17, GPIO.HIGH)
-	GPIO.output(16, GPIO.HIGH)
-	GPIO.output(20, GPIO.LOW)
-	GPIO.output(21, GPIO.HIGH)
+def right:
+	output(MOVE_RIGHT, LOW)
 	print("right")
 
-def up():
-	GPIO.output(18, GPIO.HIGH)
-	GPIO.output(17, GPIO.HIGH)
-	GPIO.output(16, GPIO.HIGH)
-	GPIO.output(20, GPIO.HIGH)
-	GPIO.output(21, GPIO.LOW)
+def up:
+	output(MOVE_UP, LOW)
 	print("up")
 
-def nothing():
-	GPIO.output(18, GPIO.HIGH)
-	GPIO.output(17, GPIO.HIGH)
-	GPIO.output(16, GPIO.HIGH)
-	GPIO.output(20, GPIO.HIGH)
-	GPIO.output(21, GPIO.HIGH)
-	print("nothing")	
+def reset:
+	for m in MOVEMENTS:
+		output(m, HIGH)
+	print("nothing")
 
+def init(opt):
+	# load the object detection network
+	net = detectNet(opt.network, argv, opt.threshold)
+	is_headless = ["--headless"] if argv[0].find('console.py') != -1 else [""]
+	# create video sources & outputs
+	video_source = videoSource(opt.input_URI, argv = argv)
+	video_output = videoOutput(opt.output_URI, argv = argv + is_headless)
+	
+	#setup GPIO pins
+	setmode(BCM) #RaspPi pin numbering
+	
+	for m in MOVEMENTS:
+		setup(m, OUT, initial = HIGH)
+	return net, video_source, video_output
 
-
-# declare variables as global and that
-
-global index
-global width
-global location
-global confidence
-index = 0
-width = 0
-location = 0
-condifence = 0;
-
-# process frames until the user exits
-while True:	
-
+def loop(opt, net, video_source, video_output):
+	# process frames until the user exits
 	# capture the next image
-	img = input.Capture()
+	img = video_source.Capture()
 
 	# detect objects in the image (with overlay)
-	detections = net.Detect(img, overlay=opt.overlay)
+	detections = net.Detect(img, overlay = opt.overlay)
 
 	# print the detections
-	#print("detected {:d} objects in image".format(len(detections)))
+	# print(f"detected {len(detections):d} objects in image")
 
-	# check for detections, otherwise nothing	
+	# check for detections, otherwise nothing
 
 	if(len(detections) > 0):
+		detection, *_ = detections
 		print("object detected")
-		for detection in detections:
-			index = detections[0].ClassID
-			confidence = (detections[0].Confidence)
-			width = (detections[0].Width)
-			location = (detections[0].Center[0])
+		index = detection.ClassID
+		confidence = (detection.Confidence)
 
-			# print index of item, width and horizonal location
+		# print index of item, width and horizonal location
 
-			print(index)
-			print(width)
-			print(location)
-			print(confidence)
+		print(index, detection.Width, detection.Center[0], confidence)
 
-			# look for detections
+		# look for detections
 
-			if (index == 1 and confidence > 0.9):
-				back()
-
-			elif (index == 2 and confidence > 0.7):
-				forward()
-
-			elif (index == 3 and confidence > 0.7):
-				left()
-
-			elif (index == 4 and confidence > 0.7):
-				right()
-
-			elif (index == 5 and confidence > 0.7):
-				up()
-
+		if (index == 1 and confidence > 0.9): back()
+		elif (index == 2 and confidence > 0.7): forward()
+		elif (index == 3 and confidence > 0.7): left()
+		elif (index == 4 and confidence > 0.7): right()
+		elif (index == 5 and confidence > 0.7): up()
 	else:
-		nothing()	# nothing is detected
-
-
+		reset()	# nothing is detected
+	
 	# render the image
-	output.Render(img)
-
+	video_output.Render(img)
+	
 	# update the title bar
-	output.SetStatus("{:s} | Network {:.0f} FPS".format(opt.network, net.GetNetworkFPS()))
-
+	video_output.SetStatus(
+		f"{opt.network:s} | Network {net.GetNetworkFPS():.0f} FPS")
+	
 	# print out performance info
 	#net.PrintProfilerTimes()
-
+	
 	# exit on input/output EOS
-	if not input.IsStreaming() or not output.IsStreaming():
+	if not video_source.IsStreaming() or not video_output.IsStreaming():
 		break
 
-
+if __name__ == "__main__":
+	# parse the command line
+	parser = ArgumentParser(
+		description = "Locate objects in a live camera stream using an object detection DNN.",
+		formatter_class = RawTextHelpFormatter,
+		epilog = detectNet.Usage()
+			+ videoSource.Usage()
+			+ videoOutput.Usage()
+			+ logUsage()
+	)
+	options = [
+		("input_URI", {"type": str, "default": '', "nargs": '?', "help": "URI of the input stream"}),
+		("output_URI", {"type": str, "default": '', "nargs": '?', "help": "URI of the output stream"}),
+		("--network", {"type": str, "default": "ssd-mobilenet-v2", "help": "pre-trained model to load (see below for options)"}),
+		("--overlay", {"type": str, "default": "box,labels,conf", "help": "detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'"}),
+		("--threshold", {"type": float, "default": 0.5, "help": "minimum detection threshold to use"})]
+	for var, option in options:
+		parser.add_argument(var, **option)
+	try:
+		opt = parser.parse_known_args()[0]
+	except:
+		print('')
+		parser.print_help()
+		exit(0)
+	net, video_source, video_output = init(opt)
+	while True:
+		loop(opt, net, video_source, video_output)
